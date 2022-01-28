3b2da68120753c3b954e8dcd67654efa5675ffdd
Willian Souza <willianolsouza@hotmail.com>
2022-01-28 09:40:19 -0300

    Projeto spring boot finalizado

--- src/test/java/br/com/alura/forum/ForumApplicationTests.java
+++ src/test/java/br/com/alura/forum/ForumApplicationTests.java
@@ -1,13 +1,19 @@
 package br.com.alura.forum;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.util.Assert;
 
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 class ForumApplicationTests {
 
+	@SuppressWarnings("deprecation")
 	@Test
 	void contextLoads() {
+		Assert.notNull(true);
 	}
 
 }
